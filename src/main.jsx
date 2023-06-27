cd28eb937c6e64919b1eaf07c1a7b47ccbf6e426
Pichborith Kong <pichborith@gmail.com>
2023-06-26 17:55:37 -0700

    update bryson work

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,50 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import Root from './root';
+import Home from './routes/Home/Home';
+import Featured from './routes/Featured/Featured';
+import Women from './routes/Women/Women';
+import Men from './routes/Men/Men';
+import Kids from './routes/Kids/Kids';
+import Register from './routes/Register.jsx/Register';
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/featured',
+        element: <Featured />,
+      },
+      {
+        path: '/women',
+        element: <Women />,
+      },
+      {
+        path: '/men',
+        element: <Men />,
+      },
+      {
+        path: '/kids',
+        element: <Kids />,
+      },
+      {
+        path: '/register',
+        element: <Register />,
+      },
+    ],
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ src/routes/Men/Men.jsx
@@ -0,0 +1,7 @@
+export default function Men() {
+  return (
+    <div>
+      <h1>Mens Clothes</h1>
+    </div>
+  );
+}
