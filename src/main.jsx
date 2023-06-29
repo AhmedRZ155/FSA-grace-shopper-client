9f6e0d3bfc5ac05ed5c8fd1087ff119585cf8a67
brysonts <114453821+brysonts@users.noreply.github.com>
2023-06-28 18:47:08 -0600

    registerCommit

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
-import Root from './root';
-import Home from './routes/Home/Home';
-import Featured from './routes/Featured/Featured';
-import Women from './routes/Women/Women';
-import Men from './routes/Men/Men';
-import Kids from './routes/Kids/Kids';
-import Register from './routes/Register.jsx/Register';
+import Root from './root'
+import Home from './routes/Home/Home'
+import Featured from './routes/Featured/Featured'
+import Women from './routes/Women/Women'
+import Men from './routes/Men/Men'
+import Kids from './routes/Kids/Kids'
+import Register from './routes/Register/Register'
+import Login from './routes/Login/login'
 const router = createBrowserRouter([
   {
     path: '/',
@@ -38,13 +39,17 @@ const router = createBrowserRouter([
         path: '/register',
         element: <Register />,
       },
+      {
+        path: '/login',
+        element: <Login />,
+      },
     ],
   },
-]);
+])
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
--- src/routes/Register.jsx/Register.jsx
+++
@@ -1,7 +0,0 @@
-export default function Register() {
-  return (
-    <div>
-      <h1>Register Here</h1>
-    </div>
-  );
-}
